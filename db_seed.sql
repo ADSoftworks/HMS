d8341076496b8ee02e79e51a94b73b6822d2c6a9
bobdesaunois <bobdesaunois@gmail.com>
2014-09-15 09:34:42 +0200

    Missing database seed statements
    Fixed.

--- db_seed.sql
+++ db_seed.sql
@@ -16,6 +16,10 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
+CREATE DATABASE homework;
+
+USE homework;
+
 --
 -- Database: `homework`
 --
