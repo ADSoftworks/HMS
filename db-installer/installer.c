af611df6545c4c574285399eb9f872335f9f0e5d
bobdesaunois <bobdesaunois@gmail.com>
2014-10-02 21:47:40 +0200

    Removed some un necessary output in the installer

--- db-installer/installer.c
+++ db-installer/installer.c
@@ -2,8 +2,6 @@
 
 void installer () {
 
-	puts ("Now attempting to log into MySQL with root.");
-	puts ("Please enter it's password.");
 	system ("mysql -u root -ptoor -e 'SOURCE db_seed.sql'");
 	puts ("##################"
 		  "\nINSTALLING IS DONE"
